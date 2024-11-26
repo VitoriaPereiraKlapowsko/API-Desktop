37a28facf361c1da56d58c71d38ab693ae5d4579
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-11-26 15:23:31 -0300

    Finalizando a atividade

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,39 @@ import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import apiRoutes from './routes/routes';
-
+import rateLimit from 'express-rate-limit'; // Middleware para limitar as requisições
+import winston from 'winston'; // Para logs
 dotenv.config();
 
 const server = express();
 
+// Logger de auditoria com o Winston
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'audit.log' }), // Arquivo de log de auditoria
+    ],
+  });
+
+  // Limitando as requisições para evitar ataques DDoS
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, // Limita a 100 requisições por IP
+    message: 'Limite de requisições excedido. Tente novamente mais tarde.',
+  });
+
 server.use(cors());
+
+// Middleware para limitação de requisições
+server.use(limiter);
+
+// Middleware para registrar as requisições recebidas
+server.use((req: Request, res: Response, next) => {
+    const { method, url } = req;
+    logger.info(`Recebida requisição: ${method} ${url}`); // Log da requisição
+    next();
+  });
 
 server.use(express.static(path.join(__dirname, '../public')));
 
